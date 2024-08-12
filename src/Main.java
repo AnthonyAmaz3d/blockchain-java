8e2be20496dfb9dfba78bc45a363a406463487f7
AnthonyAmaz3d <anthonymaia00@gmail.com>
2024-08-12 15:55:50 -0300

    adicionando os blockchains na arraylist e visualizando como json

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 public class Main {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        Block firstBlock = new Block("Primeira mensagem", "0");
-        System.out.println("Hash para o block 1 :" + firstBlock.hash);
+        blockchain.add(new Block("Testando o primeiro block com arraylist", "1"));
+        blockchain.add(new Block("Testando o segundo block com arraylist", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("Testando o terceiro block com arraylist", blockchain.get(blockchain.size()-1).hash));
 
-        Block secondBlock = new Block("Segunda mensagem", firstBlock.hash);
-        System.out.println("Hash para o block 2 :" + secondBlock.hash);
-
-        Block thirdBlock = new Block("Terceira mensagem", secondBlock.hash);
-        System.out.println("Hash para o block 3 :" + thirdBlock.hash);
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
     }
 }
